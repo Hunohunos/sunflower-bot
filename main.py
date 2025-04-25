acbe5e9e5d762cd142fecb0eb4ce37a12762ba9c
Hunohunos <izzoerian@gmail.com>
2025-04-25 15:20:11 +0800

    again

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
+from keep_alive import keep_alive
+keep_alive()
+
 import discord
 from discord.ext import commands
 import random
 import os
-import asyncio
-from flask import Flask
 
 # Store previous messages
 message_history = []
 
-# Initialize Flask app for keeping the service alive
-app = Flask(__name__)
-
-# Set up the Discord bot
+# Set up bot intents and instance
 intents = discord.Intents.default()
 intents.message_content = True  # Required to read messages
 intents.messages = True
-
 bot = commands.Bot(command_prefix="!", intents=intents)
-
-@app.route("/")
-def home():
-    return "Bot is alive"
 
 @bot.event
 async def on_ready():
@@ -53,15 +46,5 @@ async def on_message(message):
 async def ping(ctx):
     await ctx.send("Pong!")
 
-# Async function to start the bot and the Flask server
-async def start_bot():
-    # Start the Flask app in the background
-    loop = asyncio.get_event_loop()
-    loop.create_task(bot.start(os.environ["DISCORD_TOKEN"]))
-
-    # Run Flask app (this will keep the bot alive)
-    app.run(host="0.0.0.0", port=8080)
-
-# Running both the Flask web server and the Discord bot with asyncio
-if __name__ == "__main__":
-    asyncio.run(start_bot())
+# Run the bot with your token from the environment variable
+bot.run(os.environ["DISCORD_TOKEN"])
